6d3b0a33f708c6e2ceab0247754b0b60db6b244d
Marina <do-you-speak@yandex.ru>
2016-09-04 02:05:30 +0300

    Add some styles

--- all.js
+++ all.js
@@ -2,21 +2,27 @@ window.onload = function() {
   var canvas = document.getElementById("myCanvas");
   var context = canvas.getContext("2d");
   var lnk = document.getElementById("downloadLink");
+  lnk.href = canvas.toDataURL();
+  var textInput = document.getElementById('pictureText');
   var pictureText = document.getElementById('pictureText').value;
   var imageObj = new Image();
-  var btn = document.getElementById("saveButton");
   var state = "initial";
-  btn.addEventListener("click", function() {
+  textInput.addEventListener("input", function() {
     placeText();
   });
+
+  textInput.addEventListener("change", function() {
+    setDownload();
+  })
   imageObj.onload = function(){
     if (state == "initial") {
       context.beginPath();
       context.clearRect(0, 0, canvas.width, canvas.height);
       context.drawImage(imageObj, 0, 0);
-      context.font = "20px Calibri";
-      context.fillStyle = 'green';
-      context.fillText("Your text here", 120, 50);
+      context.font = "20px Roboto";
+      context.fillStyle = '#767676';
+      context.textAlign="center";
+      context.fillText("Your text here", canvas.width/2, 50);
       context.save();
     }
   };
@@ -33,13 +39,13 @@ window.onload = function() {
       imageObj.src = "https://pbs.twimg.com/profile_images/616542814319415296/McCTpH_E_400x400.jpg";
       imageObj.setAttribute("crossOrigin", "Anonymous");
       pictureText = document.getElementById('pictureText').value;
-      context.font = "20px Calibri";
-      context.fillStyle = 'green';
-      context.fillText(pictureText, 120, 50);
+      context.fillText(pictureText, canvas.width/2, 50);
       context.save();
-
-      var newCanvas = document.getElementById("myCanvas");
-      lnk.href = newCanvas.toDataURL();
     }
+  }
+
+  function setDownload() {
+    var newCanvas = document.getElementById("myCanvas");
+    lnk.href = newCanvas.toDataURL();
   }
 };
